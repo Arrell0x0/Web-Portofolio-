7ac10eebdacb68fdb4a6d3cd6d78546eb95bbc52
Arrell0x0 <darrellch.huang@gmail.com>
2026-05-31 10:40:42 +0800

    update website

---
+++ index.js
@@ -0,0 +1,57 @@
+// const nama = prompt("masukan nama anda!");
+// let umur = prompt("masukan umur anda!");
+
+// function tampilkan() {
+//   alert("ini nama orang   " + nama + "  umurnya  " + umur);
+// }
+
+// const res = document.querySelector(".ress");
+// const btn = document.querySelector(".btn");
+// const input2 = document.querySelector(".input2");
+
+// btn.addEventListener("click", function () {
+  // alert("halo");
+//   ress.innerHTML="<h1>halo</h1>"
+// });
+
+// btn.addEventListener("click", function () {
+//   res.textContent=
+// });
+
+// function tambah(){
+//     res.innerHTML=<h1> halo </h1>
+// }
+// function hitung(a, b, c) {
+//   let hasilAwal = a + b;
+//   let hasilAkhir = hasilAwal - c;
+//   return hasilAkhir;
+// }
+
+// res.textContent = hitung(
+//   prompt("Enter a number"),
+//   prompt("Enter another number"),
+//   prompt("Enter Another number untuk subtract"),
+// );
+
+// let array = [1, 2, 3, 4, 5];
+
+// console.log(array[0]);
+
+// let data = ["PUBG", "FF", "ML", "Roblox", "CS2"];
+
+// data.sort();
+
+// console.log(data.length);
+// console.log(data);
+
+// let dataDiri = {
+//   nama: "Darrell",
+//   hobi: "main game",
+// };
+
+// console.log(dataDiri);
+
+// let nomor = [1, 2, 3, 4, 5];
+
+// nomor.sort((a) => a - 1);
+// console.log(nomor);
